49b866b68f7051b3680aeb411abb95c0c86e5503
Ryogo Kanatani <ryogok@outlook.com>
2018-01-20 11:57:21 +0900

    Add xcode project

---
+++ AppCenterTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppCenterTestApp
+//
+//  Created by Ryogo Kanatani on 2018/01/20.
+//  Copyright © 2018 Ryogo Kanatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AppCenterTestApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AppCenterTestApp
+  pod 'AppCenter'
+
+  target 'AppCenterTestAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AppCenterTestAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
